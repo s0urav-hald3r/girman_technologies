0daffa051604370da043688b72fb60fb8d3c590d
Sourav Halder <bishalder96@gmail.com>
2024-10-01 21:11:12 +0530

    app running successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:girman/app.dart';
 
+Future<void> main() async {
+  // ! Add Widgets Binding
+  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // ! Await Native Splash
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // ! Fixed Portrait Orientation
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const App());
+
+  // ! Remove Native Splash
+  // FlutterNativeSplash.remove();
+}
